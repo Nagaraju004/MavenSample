1aad19bc012127c19cddf5d313d764c1c07fa9a5
Nagaraju004 <34675915+Nagaraju004@users.noreply.github.com>
2023-01-24 16:12:51 +0530

    Updated last println in app.java

--- src/main/java/com/naga/MavenSample/App.java
+++ src/main/java/com/naga/MavenSample/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "This is sample project!!!" );
+        System.out.println( "This is sample project" );
     }
 }
